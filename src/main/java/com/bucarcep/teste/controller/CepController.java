0110022a4dd2d2555effd612b03a6b910af4b8ca
ewertonalves <ewertonalves725@gmail.com>
2024-10-23 16:17:52 -0300

    retornado @param

--- src/main/java/com/bucarcep/teste/controller/CepController.java
+++ src/main/java/com/bucarcep/teste/controller/CepController.java
@@ -3,8 +3,8 @@ package com.bucarcep.teste.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bucarcep.teste.model.MessageResponse;
@@ -21,7 +21,7 @@ public class CepController {
 	}
 	
 	@GetMapping(value = "/{cep}", consumes = "application/json; charset=UTF-8")
-	public ResponseEntity<MessageResponse> buscarCep(@RequestParam(value = "cep") String cep){
+	public ResponseEntity<MessageResponse> buscarCep(@PathVariable String cep){
 		try {
 			service.buscarCep(cep);
 			return new ResponseEntity<MessageResponse>(new MessageResponse("Cep localizado: " + cep), HttpStatus.OK);
